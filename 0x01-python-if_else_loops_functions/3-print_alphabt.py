136393a3ff6e288dd79d45bafee94fa9d4306ac3
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-12 12:04:28 -0700

    4 tasks added to test checker

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z')):
+    if i == 101:
+        pass
+    print("{:c}".format(i), end="")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This folder contains the tasks for statements and loops in python
