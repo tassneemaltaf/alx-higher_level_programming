bda7c7d9640fce7060619b126930fc1e641aaf4f
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-13 01:52:26 -0700

    Some tasks added

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')):
-    if chr(i) == 'e' or chr(i) == 'z' :
-        pass
-    print("{:c}".format(i), end="")
+for i in range(97, 123):
+    if chr(i) != 'e' and chr(i) != 'z' :
+        print("{:c}".format(i), end="")
