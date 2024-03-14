2628a6a1c0f0bd51940cbc5b5fe79e6b639540e5
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-14 13:26:04 -0700

    testing taks 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys
-print("{} arguments:".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name == "__main__":
+    import sys
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
