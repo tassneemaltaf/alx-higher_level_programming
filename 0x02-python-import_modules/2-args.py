9fd68597cabc532a54c967937673d6c50088a3fb
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-14 13:48:31 -0700

    Testing task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments:".format(len(sys.argv) - 1))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments")
+    elif count == 1:
+        print("1 argument")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
