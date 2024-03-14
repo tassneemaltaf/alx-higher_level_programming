7529b220c45cf8d1cf391dfbb8f37d33b70def28
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-14 13:30:01 -0700

    Testing task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-for i in range(1, len(sys.argv)):
-    sum += int(sys.argv[i])
-print("{}".format(sum))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+    print("{}".format(sum))
