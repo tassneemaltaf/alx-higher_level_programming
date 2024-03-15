3ccf07d486f23ec21395d160b9b7432802c99fc2
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-14 21:00:04 -0700

    Task 2 edited

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(count))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
