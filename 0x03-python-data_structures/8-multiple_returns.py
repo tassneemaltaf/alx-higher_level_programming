c60d5bb17c679eca0057bef4d34b7ae8c644ca62
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-19 07:46:26 -0700

    Task 8 edit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if (sentence == ""):
-        return ()
+        return None
     length = len(sentence)
     first = sentence[0]
     tup = (length, first)
