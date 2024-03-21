52876b78a37360299a61436dffebe883b1be773a
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-21 01:00:31 -0700

    Testing task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_dif = len(tuple_a) - len(tuple_b)
-    if len_dif > 0:
-        tuple_b += (0,) * len_dif
-    elif len_dif < 0:
-        tuple_a += (0,) * len_dif
-    return tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    if len(tuple_a) > 2:
+        tuple_a -= (0,) * (len(tuple_a) - 2)
+    elif len(tuple_b) > 2:
+        tuple_b -= (0,) * (len(tuple_b) - 2)
+    else:
+        len_dif = len(tuple_a) - len(tuple_b)
+        if len_dif > 0:
+            tuple_b += (0,) * len_dif
+        elif len_dif < 0:
+            tuple_a += (0,) * len_dif
+        return tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
