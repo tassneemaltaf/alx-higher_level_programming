7ede1b7080ac32513a0c90fe20a960df4271caef
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-25 11:52:30 -0700

    Task 7 try 4

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary.keys():
+    if key in a_dictionary:
         a_dictionary[key] = value
-    elif key not in a_dictionary.keys():
-        a_dictionary.update(key: value)
+    else:
+        a_dictionary.update({key: value})
 
     for i, j in a_dictionary.items():
         print("{}: {}".format(i, j))
