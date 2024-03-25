724427b13ade59d4cc10dd58c0de23579968cef5
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-25 12:05:48 -0700

    Task 8 edited

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,4 @@ def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
 
-    for i, j in a_dictionary.items():
-        print("{}: {}".format(i, j))
+    return a_dictionary
