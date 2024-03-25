9eb976c087a5d650ae0b0a0db2b4e9c1f2a328c7
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-25 11:44:38 -0700

    Task 7 added

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return None
     a_keys = list(a_dictionary.keys())
     a_keys.sort()
     for i in a_keys:
