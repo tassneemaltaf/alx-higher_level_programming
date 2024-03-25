e2a301140e6abd50890850d0febaac510a3884cb
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2024-03-25 11:48:00 -0700

    line 7 edited

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary.keys():
         a_dictionary[key] = value
     else:
-        a_dictionary.update(key = value)
+        a_dictionary.update(key: value)
     for i, j in a_dictionary.items():
         print("{}: {}".format(i, j))
